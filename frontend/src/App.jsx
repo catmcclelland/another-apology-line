1760431084bf2254406339f8cd61d7ec4053bbae
Cat <84889884+catmcclelland@users.noreply.github.com>
2022-05-20 22:19:48 -0400

    firebase fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,28 +9,7 @@ import {
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 function App() {
-  const [data, setData] = useState([]);
-  const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-  const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-
-  const [loggedIn, setLoggedIn] = useState(true);
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Recordings.json`,
-        {
-          auth: {
-            username: TWILIO_ACCOUNT_SID,
-            password: TWILIO_AUTH_TOKEN,
-          },
-        }
-      )
-      .then((res) => {
-        setData(res.data);
-      });
-  }, []);
-
-  console.log(data);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <Router>
--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import App from "./App";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAtbXwZJr7G34pW1ksIfrx1oSCvbHW25x0",
   authDomain: "apology-line.firebaseapp.com",
@@ -15,7 +15,10 @@ const firebaseConfig = {
   appId: "1:724605836134:web:9046af3f00e13a23c046a7",
   measurementId: "G-XHMJTRHN95",
 };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
 const analytics = getAnalytics(app);
